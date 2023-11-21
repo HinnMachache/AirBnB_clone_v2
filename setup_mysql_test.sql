8b26cf77edf91302c96e5228f5b84beb6c2c6782
Ernest-Geek <ernestampene1@gmail.com>
2023-11-21 18:36:05 +0000

    Fix all errors to pass checker

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
----Script that prepares the test database server for my airbnb project
+-- Script that prepares the test database server for my airbnb project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'Cable@123';
 USE hbnb_test_db;
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
@@ -8,7 +8,7 @@ FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'Cable@123';
 USE hbnb_test_db;
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
