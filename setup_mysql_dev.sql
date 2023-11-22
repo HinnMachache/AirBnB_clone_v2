3da1f05c75771c404c369fedd2c0e12950112c60
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-22 20:01:35 +0000

    d

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,6 @@
----Script that prepares the database server for my airbnb project
---This query creates the db
+--Script to set up my db 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---This query creates the user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---This query grants all privileges to user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
---This query grants select privileges to user
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
---This query flushes the qrants
+GRANT ALL PRIVILEGES ON  hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
