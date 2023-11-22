219b3ca9c584ca2f0b70ad972ef8f14ddab49a6b
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-22 21:31:58 +0000

    Adding remainder

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -12,3 +13,9 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
