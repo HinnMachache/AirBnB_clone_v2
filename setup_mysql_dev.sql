7057bbec07113e09231c62f60c808bc90364f198
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-22 19:53:16 +0000

    d

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,8 +3,6 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 --This query creates the user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---This query uses the db for subsquent operations
-USE hbnb_dev_db;
 --This query grants all privileges to user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 --This query grants select privileges to user
