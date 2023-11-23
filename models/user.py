c6a32e91a9c74f20a686364705566e4519014503
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-23 08:44:41 +0000

    Fixing task

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -16,6 +17,3 @@ class User(BaseModel, Base):
 
     places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="user")
-    
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
