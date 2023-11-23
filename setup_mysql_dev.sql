0ed412581f7a845975c3d6d50373307ccb22c6b9
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-23 09:13:06 +0000

    R

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,18 @@
 --Script to set up my db 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+
+USE hbnb_dev_db;
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 USE hbnb_dev_db;
