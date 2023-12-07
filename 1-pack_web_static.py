a1e383bd00eb7259bdff6cacec0c2c77cbe3dd98
Ayine-nongre <eugeneatinbire@gmail.com>
2023-12-07 11:10:21 +0000

    Fixing pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-This is a script that generates a .tgz archive from the contents of the web_static folder
+This is a script that generates a .tgz archive
+from the contents of the web_static folder
 """
 
 
 import os
 import datetime
 from fabric.api import local, runs_once
+
 
 @runs_once
 def do_pack():
@@ -15,14 +17,15 @@ def do_pack():
 
     date = datetime.now()
     dest = "versions/web_static{}{}{}{}{}{}.tgz".format(
-        date.year,date.month,date.day,
-        date.hour,date.minute,date.second
+        date.year, date.month, date.day,
+        date.hour, date.minute, date.second
     )
 
     try:
         print("Packing web_static to {}".format(dest))
         local("tar -czvf {} web_static".format(dest))
-        print("web_static packed: {} -> {}Bytes".format(dest, os.stat(dest).st_size))
+        print("web_static packed: {} -> {}Bytes".format(dest,
+                                                        os.stat(dest).st_size))
     except Exception as e:
         dest = None
     return dest
