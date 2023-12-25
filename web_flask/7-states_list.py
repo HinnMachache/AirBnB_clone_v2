0072a782ba2a92bb39ed5bfdd488e691c98ced5e
Ayine-nongre <eugeneatinbire@gmail.com>
2023-12-25 20:16:17 +0000

    Airbnb v2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,6 +16,7 @@ app = Flask(__name__)
 def states_list():
     # This function returns a list of states
     data = storage.all(State)
+    data = sorted(data.values(), key=lambda data:data.name)
     return render_template('7-states_list.html', data=data)
 
 
